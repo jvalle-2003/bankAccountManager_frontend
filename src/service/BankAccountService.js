cd890749dfc46f899ac05c124b53708b976e6140
Fredy Gonzalez <fmgc94@gmail.com>
2026-03-18 22:58:36 -0600

    repair port routes

--- src/service/BankAccountService.js
+++ src/service/BankAccountService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // URL base de tu backend (cámbiala si es diferente)
-const API_URL = 'http://localhost:3000/api'; 
+const API_URL = 'http://localhost:3001/api'; 
 
 export const BankAccountService = {
     // ==========================================
